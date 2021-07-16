3b9cad8322400544181f54b4e78f2ac1eac562cf
kimin0402 <renzo0909@gmail.com>
2021-07-17 02:03:10 +0900

    commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 # List of requirements
-requirements = []  # This could be retrieved from requirements.txt
+requirements = ['pysam>=0.16.0.1', 'cyvcf2>=0.30.1']  # This could be retrieved from requirements.txt
 
 # Package (minimal) configuration
 setup(
